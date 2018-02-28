151211cc8d0a5e2b6b6c1a0fbd8a27f15e4dd043
Ahmad Arif <ahmad.arif019@gmail.com>
2018-02-28 15:17:07 +0700

    eksplorasi HTTP Lifecycle Adonis

--- start/routes.js
+++ start/routes.js
@@ -16,3 +16,57 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+const User = use('App/Models/User')
+
+Route.get('/api/users/:id', async ({ params }) => {
+  const user = await User.find(params.id)
+  return user
+})
+
+Route.get('/sample/:type', async ({ params, response }) => {
+  if (params.type) {
+    return response.send({ message: 'Tipe 1'})
+  }
+  return response.send({ message: 'Bukan tipe 1'})
+})
+
+function contohCallback(value, callback) {
+  setTimeout(() => {
+      callback(null, value * 2)
+  }, 1000)
+}
+
+// Contoh callback
+function contohCallback(value, callback) {
+  setTimeout(() => {
+      callback(null, value * 2)
+  }, 1000)
+}
+
+Route.get('/callback/:value', async ({ params, response }) => {
+  const value = params.value
+  contohCallback(value, function(err, data) {
+    console.log('hoho')
+    response.send(`Hasil = `, data)
+  })
+})
+    
+const Helpers = use('Helpers')
+Route.get('/callback/v1/:value', async ({ params, response }) => {
+  const value = params.value
+  const contohCallbackPromisify = Helpers.promisify(contohCallback)
+
+  const data = await contohCallbackPromisify(value)
+  response.send(`Hasil = ${data}`)
+})
+
+const { promisify } = use('util')
+Route.get('/callback/v2/:value', async ({ params, response }) => {
+  const value = params.value
+  const contohCallbackPromisify = promisify(contohCallback)
+
+  const data = await contohCallbackPromisify(value)
+  response.send(`Hasil = ${data}`)
+})
+//> Contoh callback
